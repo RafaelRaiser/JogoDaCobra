d49d0c52f3d509c35c1927c3bc81dbbff87257b0
RafaelRaiser <rafabarrosraiser@gmail.com>
2024-10-08 13:54:46 -0300

    UI

---
+++ Jogo da Cobra/Assets/Script/UIManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class UIManager : MonoBehaviour
+{
+    public Slider speedSlider;
+    public InputField gridSizeInput;
+    public Text speedValueText;
+
+    void Start()
+    {
+        // Inicializa valores padrões na UI
+        speedSlider.onValueChanged.AddListener(delegate { UpdateSpeedValue(); });
+    }
+
+    void UpdateSpeedValue()
+    {
+        speedValueText.text = speedSlider.value.ToString("F2");
+    }
+
+    public void StartGame()
+    {
+        // Define as configurações do jogo a partir da UI
+        int gridSize = int.Parse(gridSizeInput.text);
+        GameManager.instance.gridSize = gridSize;
+        SceneManager.LoadScene("GameScene");
+    }
+}
