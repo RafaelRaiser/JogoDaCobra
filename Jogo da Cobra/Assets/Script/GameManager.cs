11db8e711e70935dcc030dcb855cc44b9ade0d12
RafaelRaiser <rafabarrosraiser@gmail.com>
2024-10-08 13:33:43 -0300

    Update GameManager.cs

--- Jogo da Cobra/Assets/Script/GameManager.cs
+++ Jogo da Cobra/Assets/Script/GameManager.cs
@@ -1,19 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class GameManager : MonoBehaviour
 {
-    public static GameManager instance; // Instância única
+    public static GameManager instance;
+    public int gridSize = 20;
+    public GameObject foodPrefab;
+
     #region Singleton
     void Awake()
     {
         if (instance == null)
+        {
             instance = this;
+        }
         else
+        {
             Destroy(gameObject);
-
-        DontDestroyOnLoad(gameObject); // Mantém o objeto ao mudar de cena
+        }
     }
     #endregion
+
+    void Start()
+    {
+        // Inicializa a comida
+        SpawnFood();
+    }
+
+    public void SpawnFood()
+    {
+        // Spawna a comida em um ponto aleatório do grid
+        Instantiate(foodPrefab);
+    }
 }
 
